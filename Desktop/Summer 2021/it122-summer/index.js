dfc4aafb30db0ddcc9f310aabdaa625f78268e59
Diego-Cano <64940291+Diego-Cano@users.noreply.github.com>
2021-07-19 19:41:53 -0700

    Move homework 3 to project root

--- Desktop/Summer 2021/it122-summer/index.js
+++ Desktop/Summer 2021/it122-summer/index.js
@@ -1,31 +1,40 @@
-import http from 'http';
-import { parse } from "querystring";
-import { getAll, getItem } from './data.js';
+import * as southamerica from "./lib/southamerica.js";
+import express from 'express';
+import handlebars from 'express-handlebars';
 
+const app = express();
 
+app.set("port", process.env.PORT || 3000);
+app.use(express.static('./public'));
+app.use(express.urlencoded());
+app.use(express.json());
 
-http.createServer((req,res) => {
-    var path = req.url.toString();
-    let url = req.url.split("?");  // separate route from query string
-    let query = parse(url[1]);
-    var path = url[0].toLowerCase();
-    switch(path) {
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(JSON.stringify(getAll()));
-            break;
-        case '/detail':
-            let found = getItem(query.name);
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(JSON.stringify(found));
-            break;
-        case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('About page: Hello! My name is Diego Cano, and Im from Venezuela. Ive lived in Seattle for two years. I am a college student at Seattle Central College in the IT department. I have experience working with people because Ive worked as a waiter, barista, and banquet server. Id like to obtain my ASS-T in programming and try to transfer to a four-year college or university. My goal is to work for a company in the It team or as a freelancer.');
-            break;
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not found');
-            break;
-    }
-}).listen(process.env.PORT || 3000);
+app.engine('hbs', handlebars({defaultLayout: "main.hbs"}));
+app.set("view engine", "hbs");
+
+app.get('/', (req,res) => {
+    res.render('home', {southamerica: southamerica.getAll()});
+});
+
+// send plain text response
+app.get('/about', (req,res) => {
+    res.type('text/plain');
+    res.send('About page: Hello! My name is Diego Cano, and Im from Venezuela. Ive lived in Seattle for two years. I am a college student at Seattle Central College in the IT department. I have experience working with people because Ive worked as a waiter, barista, and banquet server. Id like to obtain my ASS-T in programming and try to transfer to a four-year college or university. My goal is to work for a company in the It team or as a freelancer.');
+});
+
+app.get('/detail', (req,res) => {
+    console.log(req.query)
+    let result = southamerica.getItem(req.query.country);
+    res.render('details',{country: req.query.country, result });
+});
+
+// define 404 handler
+app.use((req,res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+})
+
+app.listen(app.get('port'), () => {
+    console.log('Express started');
+})
